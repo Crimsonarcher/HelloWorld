e7205e834e4149d109ad9f89d34d33b37369d9c1
Crimsonarcher <vishwanath.donti@gmail.com>
2012-12-30 21:12:29 +0530

    1.0

---
+++ src/helloword/HelloWorld.java
@@ -0,0 +1,18 @@
+package helloword;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 1;
+		while (i<=10) {
+		System.out.println("Hello World : " + i);
+		i++;
+		}
+	}
+
+}
